72e4e7af52ac0d698f86056d53a38d69b6e3227f
Roalsa <rominaasalvato@gmail.com>
2022-09-19 13:15:11 -0300

    Desafío 190922

---
+++ Codigo.js
@@ -0,0 +1,13 @@
+//Vamos a crear una declaración mediante un alerta y variables
+let edadFechaNacimientoUsuario=prompt("Ingrese Fecha De Nacimiento");
+
+
+let anioActual=2022;
+let resta= anioActual - edadFechaNacimientoUsuario;
+console.log("Buen día!!")
+
+console.log("Resultado de la resta:" + edadFechaNacimientoUsuario);
+let restaNums= anioActual - edadFechaNacimientoUsuario;
+
+alert ("El resultado de la resta es:"+restaNums);
+console.log("Su edad es:"+restaNums);
